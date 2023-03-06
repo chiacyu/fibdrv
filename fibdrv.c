8f87e8d3238cb6c387f5b0be89e7872585545918
chiacyu <yujuan476@gmail.com>
2023-03-06 21:27:56 +0800

    push change test

--- fibdrv.c
+++ fibdrv.c
@@ -27,16 +27,16 @@ static DEFINE_MUTEX(fib_mutex);
 static long long fib_sequence(long long k)
 {
     /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel. */
-    long long f[k + 2];
+    // long long f[k + 2];
 
-    f[0] = 0;
-    f[1] = 1;
+    // f[0] = 0;
+    // f[1] = 1;
 
-    for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
-    }
+    // for (int i = 2; i <= k; i++) {
+    //     f[i] = f[i - 1] + f[i - 2];
+    // }
 
-    return f[k];
+    // return f[k];
 }
 
 static int fib_open(struct inode *inode, struct file *file)
